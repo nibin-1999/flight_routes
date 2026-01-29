fa52b82b533d0874e3c565ccf91629acaa67cfc1
Nibin R P <nibinrp99@gmail.com>
2026-01-30 00:00:56 +0530

    default project app configuration completed

--- flight_routes/flight_routes/settings.py
+++ flight_routes/flight_routes/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'routes',  # flight routes app added
 ]
 
 MIDDLEWARE = [
