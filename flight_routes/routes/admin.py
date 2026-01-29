e8e2eeb887e61c9d31ede8177553640e92b45855
Nibin R P <nibinrp99@gmail.com>
2026-01-30 00:43:29 +0530

    admin.py file code

--- flight_routes/routes/admin.py
+++ flight_routes/routes/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from .models import Airport
 
-# Register your models here.
+
+@admin.register(Airport)
+class AirportAdmin(admin.ModelAdmin):
+    # Columns shown in admin list view
+    list_display = (
+        'airport_name',
+        'airport_code',
+        'position',
+        'duration',
+        'left',
+        'right',
+    )
+
+    # Search bar fields
+    search_fields = (
+        'airport_name',
+        'airport_code',
+    )
+
+    # Filters on right sidebar
+    list_filter = (
+        'position',
+    )
+
+    # Order by airport code
+    ordering = (
+        'airport_code',
+    )
+
+    # Fields arrangement in add/edit page
+    fieldsets = (
+        ('Airport Information', {
+            'fields': (
+                'airport_name',
+                'airport_code',
+                'duration',
+            )
+        }),
+        ('Route Connections', {
+            'fields': (
+                'position',
+                'left',
+                'right',
+            )
+        }),
+    )
+
+    # Improve dropdown usability for relations
+    autocomplete_fields = (
+        'left',
+        'right',
+    )
